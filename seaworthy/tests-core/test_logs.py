07597ef430792ab2f8172dbcdd175dd06a363652
Jeremy Thurgood <jeremy@praekelt.org>
2017-08-22 15:03:45 +0200

    Some wait_for_logs_matching() tests.

--- seaworthy/tests-core/test_logs.py
+++ seaworthy/tests-core/test_logs.py
@@ -1,6 +1,9 @@
 import unittest
+from time import sleep
 
-from seaworthy.logs import EqualsMatcher, RegexMatcher, SequentialLinesMatcher
+from seaworthy.logs import (
+    EqualsMatcher, RegexMatcher, SequentialLinesMatcher,
+    wait_for_logs_matching)
 
 
 class TestEqualsMatcher(unittest.TestCase):
@@ -107,3 +110,86 @@ class TestSequentialLinesMatcher(unittest.TestCase):
             str(matcher),
             "SequentialLinesMatcher(matched=[EqualsMatcher('foo'), "
             "RegexMatcher('^bar')], unmatched=[])")
+
+
+class FakeLogsContainer:
+    """
+    A container object stub that emits canned logs.
+    """
+
+    def __init__(self, log_entries, expected_kw=None):
+        self.log_entries = log_entries
+        self._seen_logs = []
+        self.expected_kw = {} if expected_kw is None else expected_kw
+
+    def logs(self, stream=False, **kw):
+        if stream:
+            assert kw == self.expected_kw
+            return self.iter_logs()
+        assert kw['tail'] > 0
+        return b''.join(self._seen_logs[-kw['tail']:])
+
+    def iter_logs(self):
+        self._seen_logs = []
+        for wait, line in self.log_entries:
+            sleep(wait)
+            self._seen_logs.append(line)
+            yield line
+
+
+class TestWaitForLogsMatchingFunc(unittest.TestCase):
+    def test_one_matching_line(self):
+        """
+        If one matching line is logged, all is happy.
+        """
+        con = FakeLogsContainer([
+            (0, b'hello\n'),
+        ])
+        # If this doesn't raise an exception, the test passes.
+        wait_for_logs_matching(con, EqualsMatcher('hello'), timeout=0.001)
+
+    def test_one_non_matching_line(self):
+        """
+        If there's no match by the time the logs end, we raise an exception.
+        """
+        con = FakeLogsContainer([
+            (0, b'goodbye\n'),
+        ])
+        with self.assertRaises(RuntimeError) as cm:
+            wait_for_logs_matching(con, EqualsMatcher('hello'), timeout=0.001)
+        self.assertIn(
+            "Logs matching EqualsMatcher('hello') not found.",
+            str(cm.exception))
+        self.assertIn('goodbye\n', str(cm.exception))
+
+    @unittest.skip('This timeout stuff is currently broken.')
+    def test_timeout_first_line(self):
+        """
+        If we take too long to get the first line, we time out.
+        """
+        con = FakeLogsContainer([
+            (2, b'hello\n'),
+        ])
+        with self.assertRaises(TimeoutError) as cm:
+            wait_for_logs_matching(con, EqualsMatcher('hello'), timeout=1)
+        self.assertIn(
+            "Timeout waiting for logs matching EqualsMatcher('hello').",
+            str(cm.exception))
+        self.assertNotIn('hello\n', str(cm.exception))
+
+    @unittest.skip('This timeout stuff is currently broken.')
+    def test_timeout_later_line(self):
+        """
+        If we take too long to get a later line, we time out.
+        """
+        con = FakeLogsContainer([
+            (0, b'hi\n'),
+            (0.01, b'hello\n'),
+        ])
+        with self.assertRaises(TimeoutError) as cm:
+            wait_for_logs_matching(con, EqualsMatcher('hello'), timeout=0.001)
+        self.assertIn(
+            "Timeout waiting for logs matching EqualsMatcher('hello').",
+            str(cm.exception))
+        self.assertIn('hi\n', str(cm.exception))
+        self.assertNotIn('hello\n', str(cm.exception))
