d1a946e9884ea6ca8d78beb58738e856348efb8a
Jamie Hewland <jhewland@gmail.com>
2017-09-07 15:31:04 +0200

    pytest: Split away pytester tests

--- seaworthy/tests-pytest/test_fixtures_pytester.py
+++ seaworthy/tests-pytest/test_fixtures_pytester.py
@@ -1,13 +1,13 @@
-import docker
-import pytest
-
+"""
+These tests use the ``pytester`` plugin to run tests in a separate process.
+Because of this, some Docker state can conflict between the main test process
+and these new processes. For this reason, these tests have been separated from
+the others. Note that these tests produce no coverage data.
+https://docs.pytest.org/en/3.2.1/writing_plugins.html#testing-plugins
+"""
 from seaworthy.checks import docker_client
-from seaworthy.containers.base import ContainerBase
-from seaworthy.dockerhelper import DockerHelper, fetch_images
+from seaworthy.dockerhelper import fetch_images
 from seaworthy.pytest.checks import dockertest
-from seaworthy.pytest.fixtures import (
-    clean_container_fixtures, container_fixture, docker_helper_fixture,
-    image_pull_fixture)
 
 
 IMG = 'nginx:alpine'
@@ -16,11 +16,6 @@ IMG = 'nginx:alpine'
 def setup_module():
     with docker_client() as client:
         fetch_images(client, [IMG])
-
-
-# We redefine the docker_helper fixture here to have function scope so our
-# tests don't conflict.
-docker_helper = docker_helper_fixture(scope='function')
 
 
 @dockertest()
@@ -51,26 +46,6 @@ class TestDockerHelperFixture:
         result = testdir.runpytest()
         result.assert_outcomes(passed=1)
 
-    def test_setup_teardown(self):
-        """
-        The fixture should yield a setup helper, and afterwards tear down the
-        helper.
-        """
-        fixture_gen = docker_helper()
-        helper = next(fixture_gen)
-        assert isinstance(helper, DockerHelper)
-
-        # Test we can create a container; if we can the helper must be set
-        # up
-        container = helper.create_container('test', IMG)
-
-        # Test things are torn down
-        with pytest.raises(StopIteration):
-            next(fixture_gen)
-
-        with pytest.raises(docker.errors.NotFound):
-            container.reload()
-
 
 @dockertest()
 class TestImagePullFixtureFunc:
@@ -89,17 +64,6 @@ class TestImagePullFixtureFunc:
             def test_image_pull(image):
                 assert 'busybox:latest' in image.tags
         """)
-
-    def test_create_fixture(self, docker_helper):
-        """
-        We can create an fixture that pulls an image and returns an image
-        model.
-        """
-        fixture = image_pull_fixture(IMG, name='image', scope='module')
-        image = fixture(docker_helper)
-
-        assert isinstance(image, docker.models.images.Image)
-        assert IMG in image.tags
 
 
 @dockertest()
@@ -124,27 +88,6 @@ class TestContainerFixtureFunc:
 
         result = testdir.runpytest()
         result.assert_outcomes(passed=1)
-
-    def test_setup_teardown(self, docker_helper):
-        """
-        The fixture should yield a started container, and afterwards stop and
-        remove the container.
-        """
-        fixture = container_fixture(
-            ContainerBase(name='test', image=IMG), 'test')
-        fixture_gen = fixture(docker_helper)
-        container = next(fixture_gen)
-
-        assert isinstance(container, ContainerBase)
-        assert container.inner().status == 'running'
-
-        # Test things are torn down
-        with pytest.raises(StopIteration):
-            next(fixture_gen)
-
-        # Container has been stopped and removed
-        with pytest.raises(RuntimeError):
-            container.inner()
 
 
 @dockertest()
@@ -194,25 +137,3 @@ class TestCleanContainerFixturesFunc:
 
         result = testdir.runpytest()
         result.assert_outcomes(passed=2)
-
-    def test_setup_teardown(self, docker_helper):
-        """
-        The fixture should yield a started container, and afterwards stop and
-        remove the container.
-        """
-        raw_fixture, fixture = clean_container_fixtures(
-            ContainerBase(name='test', image=IMG), 'test')
-        fixture_gen = raw_fixture(docker_helper)
-        # TODO: Assert on cleaning fixture
-        container = next(fixture_gen)
-
-        assert isinstance(container, ContainerBase)
-        assert container.inner().status == 'running'
-
-        # Test things are torn down
-        with pytest.raises(StopIteration):
-            next(fixture_gen)
-
-        # Container has been stopped and removed
-        with pytest.raises(RuntimeError):
-            container.inner()
