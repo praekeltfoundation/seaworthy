cf5a7d1fbb25bf711b2348776db948ce5138ad68
Jamie Hewland <jhewland@gmail.com>
2017-10-10 10:36:50 +0200

    Make the helper resource namespace configurable

--- seaworthy/utils.py
+++ seaworthy/utils.py
@@ -1,6 +1,2 @@
-def resource_name(name, namespace='test'):
-    return '{}_{}'.format(namespace, name)
-
-
 def output_lines(raw_output, encoding='utf-8'):
     return raw_output.decode(encoding).splitlines()
