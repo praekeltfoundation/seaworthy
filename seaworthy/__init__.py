fb1ea498bf0036a35fba76912bf5965b6e9ff1ab
Jamie Hewland <jhewland@gmail.com>
2017-10-30 13:50:19 +0200

    Copy pip's version system

--- seaworthy/__init__.py
+++ seaworthy/__init__.py
@@ -2,3 +2,5 @@ from .dockerhelper import DockerHelper
 from .logs import output_lines, wait_for_logs_matching
 
 __all__ = ['DockerHelper', 'output_lines', 'wait_for_logs_matching']
+
+__version__ = '0.1.0.dev0'
