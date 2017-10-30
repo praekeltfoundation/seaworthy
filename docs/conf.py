281ee7c6f280b1b90fc8360f9e2ad5916814ab23
Jamie Hewland <jhewland@gmail.com>
2017-10-30 12:41:54 +0200

    docs: viewcode is an extension people seem to use??

--- docs/conf.py
+++ docs/conf.py
@@ -40,6 +40,7 @@ extensions = [
     'sphinx.ext.autodoc',
     'sphinx.ext.doctest',
     'sphinx.ext.intersphinx',
+    'sphinx.ext.viewcode',
 ]
 
 # Add any paths that contain templates here, relative to this directory.
